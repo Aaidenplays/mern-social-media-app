76c06eacf15d7e91fcfa1816bd084745b3c13f2e
aaidenplays <aaidenplays@yahoo.com>
2020-09-03 19:49:53 -0500

    added delete and update routes to posts

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getPosts, createPost} = require('../controllers/post');
+const {getPosts, createPost, postsByUser, postById, isPoster, deletePost, updatePost} = require('../controllers/post');
 const {requireSignin} = require('../controllers/auth');
 const { userById} = require('../controllers/user');
 const {createPostValidator} = require('../validator')
@@ -7,9 +7,15 @@ const {createPostValidator} = require('../validator')
 const router = express.Router();
 
 router.get("/",  getPosts);
-router.post("/post", requireSignin, createPostValidator, createPost);
+router.post("/post/new/:userId", requireSignin, createPost, createPostValidator);
+router.get("/posts/by/:userId", requireSignin, postsByUser);
+router.delete('/post/:postId', requireSignin, isPoster, deletePost)
+router.put('/post/:postId', requireSignin, isPoster, updatePost)
 
 //any routes containing :userId, our app will first exexcute useById
 router.param("userId", userById)
+
+//any routes containing :postId, our app will first exexcute postById
+router.param("postId", postById)
 
 module.exports = router;
